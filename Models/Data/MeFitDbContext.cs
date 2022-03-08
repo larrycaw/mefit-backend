e7433e4fdd79223acf2e68a92bdef02fd620f588
Stian Økland <stian.okland@no.experis.com>
2022-03-08 14:34:08 +0100

    feat: deploy database to Azure

---
+++ Models/Data/MeFitDbContext.cs
@@ -0,0 +1,30 @@
+﻿using MeFit.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MeFit.Models.Data
+{
+    public class MeFitDbContext : DbContext
+    {
+        public  DbSet<Address> Addresses { get; set; }
+
+        public MeFitDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>().HasData(new Address
+            {
+                Id = 1,
+                AddressLine1 = "eef",
+                AddressLine2 = "fkr",
+                AddressLine3 = "efe",
+                City =  "jfr",
+                PostalCode = "1123",
+                Contry = "fw4w"
+            });
+        }
+
+    }
+}
