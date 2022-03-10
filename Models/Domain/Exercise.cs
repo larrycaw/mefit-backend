4343bc55c97e0d1663a1ca85df08ee9f372a2d7d
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-03-10 13:12:43 +0100

    feat: add exercise

---
+++ Models/Domain/Exercise.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeFit.Models.Domain
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(300)]
+        public string Description { get; set; }
+        [MaxLength(50)]
+        public string TargetMuscleGroup { get; set; }
+        [MaxLength(50)]
+        public string ImageURL { get; set; }
+        [MaxLength(50)]
+        public string VideoURL { get; set; }
+    }
+}
