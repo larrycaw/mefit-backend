cb933b7472af919b4d34557119f7b6ad0bde8f41
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-03-10 13:13:30 +0100

    feat: add sets

---
+++ Migrations/20220310112454_AddSets.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MeFit.Migrations
+{
+    public partial class AddSets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ExerciseRepetitions = table.Column<int>(type: "int", nullable: false),
+                    ExerciseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sets_Exercises_ExerciseId",
+                        column: x => x.ExerciseId,
+                        principalTable: "Exercises",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sets_ExerciseId",
+                table: "Sets",
+                column: "ExerciseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sets");
+        }
+    }
+}
---
+++ Models/Domain/Set.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeFit.Models.Domain
+{
+    public class Set
+    {
+        public int Id { get; set; }
+        public int ExerciseRepetitions { get; set; }
+        public int ExerciseId { get; set; }
+        public Exercise Exercise { get; set; }
+    }
+}
