47b442486e6169c3e020493fb3b6c8d35b2694ad
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-03-10 13:16:16 +0100

    feat: add goals

---
+++ Models/Domain/Goal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeFit.Models.Domain
+{
+    public class Goal
+    {
+        public int Id { get; set; }
+        public DateTime ProgramEndDate { get; set; }
+        public bool Achieved { get; set; }
+        public int ProgramId { get; set; }
+        public MFProgram Program { get; set; }
+    }
+}
