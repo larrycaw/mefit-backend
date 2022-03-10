b644ebfb428ea52ceb7ae16843e21d6c5274e2e1
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-03-10 13:14:22 +0100

    feat: add workouts

---
+++ Migrations/20220310113259_AddWorkout.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MeFit.Migrations
+{
+    public partial class AddWorkout : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WorkoutId",
+                table: "Sets",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Workouts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Complete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workouts", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sets_WorkoutId",
+                table: "Sets",
+                column: "WorkoutId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sets_Workouts_WorkoutId",
+                table: "Sets",
+                column: "WorkoutId",
+                principalTable: "Workouts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sets_Workouts_WorkoutId",
+                table: "Sets");
+
+            migrationBuilder.DropTable(
+                name: "Workouts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sets_WorkoutId",
+                table: "Sets");
+
+            migrationBuilder.DropColumn(
+                name: "WorkoutId",
+                table: "Sets");
+        }
+    }
+}
---
+++ Models/Domain/Workout.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeFit.Models.Domain
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string Type { get; set; }
+        public bool Complete { get; set; }
+        public ICollection<Set> Sets { get; set; }
+    }
+}
