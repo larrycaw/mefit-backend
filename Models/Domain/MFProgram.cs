a9068af22d6069039d3041a1dbe0ce03193a0047
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-03-10 13:15:23 +0100

    feat: add programs

---
+++ Models/Domain/MFProgram.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeFit.Models.Domain
+{
+    public class MFProgram
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string Category { get; set; }
+    }
+}
