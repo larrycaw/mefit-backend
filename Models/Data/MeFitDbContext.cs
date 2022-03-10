090b0c478b372c758921af48d33332c70db1e460
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-03-10 13:17:09 +0100

    feat: add profile, add dbsets

--- Models/Data/MeFitDbContext.cs
+++ Models/Data/MeFitDbContext.cs
@@ -6,7 +6,13 @@ namespace MeFit.Models.Data
 {
     public class MeFitDbContext : DbContext
     {
-        public  DbSet<Address> Addresses { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<Set> Sets { get; set; }
+        public DbSet<Workout> Workouts { get; set; }
+        public DbSet<MFProgram> Programs { get; set; }
+        public DbSet<Goal> Goals { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
 
         public MeFitDbContext([NotNullAttribute] DbContextOptions options) : base(options)
         {
---
+++ Models/Domain/Profile.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeFit.Models.Domain
+{
+    public class Profile
+    {
+        [Required]
+        public string Id { get; set; }
+        public int GoalId { get; set; }
+        public Goal CurrentGoal { get; set; }
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+        public int ProgramId { get; set; }
+        public MFProgram Program { get; set; }
+        public int WorkoutId { get; set; }
+        public Workout Workout { get; set; }
+        public int SetId { get; set; }
+        public Set Set { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        [MaxLength(200)]
+        public string MedicalConditions { get; set; }
+        [MaxLength(100)]
+        public string Disabilities { get; set; }
+    }
+}
