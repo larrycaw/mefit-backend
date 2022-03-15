dd327947d17ce36cf1e2f9f3d9ad0d79ef33a6d3
Isak Hauknes <isak.hauknes@no.experis.com>
2022-03-15 08:48:28 +0100

    chore: DTO and file for mapping added

---
+++ Models/DTOs/Profile/ProfileCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeFit.Models.DTOs.Profile
+{
+    public class ProfileCreateDTO
+    {
+        public string Id { get; set; }
+        public int? Weight { get; set; }
+        public int? Height { get; set; }
+        [MaxLength(200)]
+        public string MedicalConditions { get; set; }
+        [MaxLength(100)]
+        public string Disabilities { get; set; }
+    }
+}
---
+++ Models/DTOs/Profile/ProfileUpdateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MeFit.Models.Domain;
+
+namespace MeFit.Models.DTOs.Profile
+{
+    public class ProfileUpdateDTO
+    {
+        public string Id { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        [MaxLength(200)]
+        public string MedicalConditions { get; set; }
+        [MaxLength(100)]
+        public string Disabilities { get; set; }
+    }
+}
