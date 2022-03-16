84b1e415d5f48ed6d997ee2b5edcc6141f19e475
Isak Hauknes <isakhauknes@gmail.com>
2022-03-16 15:29:25 +0100

    chore: added missing programId and addressId to ProfileReadDTO

--- Models/DTOs/Profile/ProfileReadDTO.cs
+++ Models/DTOs/Profile/ProfileReadDTO.cs
@@ -9,6 +9,8 @@ namespace MeFit.Models.DTOs.Profile
     {
         public int? Weight { get; set; }
         public int? Height { get; set; }
+        public int? AddressId { get; set; }
+        public int? ProgramId { get; set; }
         public int? WorkoutId { get; set; }
         public int? SetId { get; set; }
         [MaxLength(200)]
